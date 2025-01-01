785124107d38629ab518322d36c8d11cf87df249
Franklin <franklin.espinoza0226@gmail.com>
2025-01-01 18:06:11 -0500

    commenting the overwrite function tojson

--- models/laboratory.model.js
+++ models/laboratory.model.js
@@ -6,12 +6,13 @@ module.exports = mongoose => {
       },
       { timestamps: true }
     );
-  
+    /*
     schema.method("toJSON", function () {
       const { __v, _id, ...object } = this.toObject();
       object.id = _id;
       return object;
     });
+    */
   
     const Laboratory = mongoose.model("laboratory", schema);
     return Laboratory;
--- models/user.model.js
+++ models/user.model.js
@@ -10,12 +10,13 @@ module.exports = mongoose => {
         },
         { timestamps: true }
     );
-
+    /*
     schema.method("toJSON", function () {
         const { __v, _id, ...object } = this.toObject();
         object.id = _id;
         return object;
     });
+    */
 
     const User = mongoose.model("user", schema);
     return User;
